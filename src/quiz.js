401494122d6db420691fee709019fb6251f74003
Jamal Powell <jamalkpowell@gmail.com>
2013-08-22 10:01:27 -0400

    Can we go over this?

--- src/quiz.js
+++ src/quiz.js
@@ -11,8 +11,33 @@ var quiz = {};
 // You can sleep in if it is not a weekday or if you are on vacation.
 // quiz.sleepIn() => false
 // quiz.sleepIn({vacation: true}) => true
+
+//////HARD WAY,that didnt work. I was trying to make an array with a bunch of days in them
+// and iterate through each and compare the input to the array.
+// var d=new Date();
+// var weekday=new Array(7);
+// weekday[0]="Sunday";
+// weekday[1]="Monday";
+// weekday[2]="Tuesday";
+// weekday[3]="Wednesday";
+// weekday[4]="Thursday";
+// weekday[5]="Friday";
+// weekday[6]="Saturday";
+
+// var n = weekday[d.getDay()];
+// quiz.sleepIn = function(options) {
+//   if
+
+// }
+
+// realized theres an easier way, there are only two days on vacation........
 quiz.sleepIn = function(options) {
-}
+  if(options === {vacation: true} || options === 'Saturday' || options === 'Sunday'){
+    return true;
+  } else {
+    return false;
+  }
+};
 
 // Question 2: function called nearHundred
 //////////////
@@ -22,6 +47,15 @@ quiz.sleepIn = function(options) {
 // quiz.nearHundred(93) => false
 // quiz.nearHundred('two') => Error: Please enter a number!
 
+quiz.nearHundred = function(options) {
+  if(options >= 90) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
 // Question 3: a function called missingChar
 //////////////
 // Remove the character that corresponds to the index from the string.
@@ -29,14 +63,37 @@ quiz.sleepIn = function(options) {
 // quiz.missingChar("kittie", 1) => "kttie"
 // quiz.missingChar(347, 1) => Error: Please enter a string!
 
+
+
 // Question 4: a function called delDel
 //////////////
 // Remove "del" from a string.
 // quiz.delDel("abdelcd") => "abcd"
 // quiz.delDel("xyz") => "xyz"
 
+quiz.delDel = function(options) {
+  var del = "delDel";
+  del.replace("Del", "");
+  return del;
+};
+
+// How do I make this stick? It keeps returning del as the original and not the replaced version.
+
 // Question 5: a method called backAround
 //////////////
 // Given a string, move the last character to the beginning.
 // "cat".backAround() => "tca"
 // "hello".backAround() => "ohell"
+
+var string = "Surf!";
+var last = string[string.length-1];
+console.log(last);
+
+backAround = function(options) {
+  var string = "Surf!";
+  var last = string[string.length-1];
+  // last to the front
+
+  new_string = last + string;
+
+};
